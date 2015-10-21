9098a67eab35315407c3c3df791683b89447b6b0
Piotr <piotr.guzik@live.com>
2015-10-21 20:01:47 +0200

    Podstawy kalkulacji na VSTO

---
+++ ExcelWorkbook2/Ribbon1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using oledb = System.Data.OleDb;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace ExcelWorkbook2
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Application _App = Globals.ThisWorkbook.Application as Excel.Application;
+            Excel.Worksheet _sheet = _App.ActiveSheet as Excel.Worksheet;
+             //Range("A5", "A5") as Excel.Range;
+            //_r.Value2 = "Alicja w krainie czarow A5";
+            DataSet1TableAdapters.v_listaKalkulacjiTableAdapter vTa = new DataSet1TableAdapters.v_listaKalkulacjiTableAdapter();
+            DataSet1.v_listaKalkulacjiDataTable dt = new DataSet1.v_listaKalkulacjiDataTable();
+            vTa.Fill(dt);
+            // Range _r = _sheet.Range["A2"];
+
+            //            _r.CopyFromRecordset()
+
+            //   Globals.Arkusz1.list1.DataSource = dt;
+            Globals.Arkusz1.list1.AutoSetDataBoundColumnHeaders = true;
+            Globals.Arkusz1.list1.SetDataBinding(dt);//, "id", "Numer Zapyania", "Indeks XL", "Numer Rysunku", "Klient", "Wielkosc Zamowienia", "Wielkosc Produkcji", "Grubosc Materialu", "Rodzaj Materialu", "Wymiar X", "Wymiar Y", "Rewizja");
+            Globals.Arkusz1.list1.columns
+            Globals.Arkusz1.list1.Disconnect();
+            // Bind the list object to the Customers table.
+            //list1.AutoSetDataBoundColumnHeaders = true;
+            //list1.DataSource = dt;
+            //list1.DataMember = "Kalkulacje";
+
+        }
+     
+    }
+}
---
+++ ExcelWorkbook2/ThisWorkbook.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Tools.Excel;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace ExcelWorkbook2
+{
+    public partial class ThisWorkbook
+    {
+        private void ThisWorkbook_Startup(object sender, System.EventArgs e)
+        {
+            wyswietlWkomorce("A1","abra kadabra");
+            Form1 frm = new Form1();
+        //    frm.ShowDialog();
+        }
+
+        public void wyswietlWkomorce(String Adres, String tekst)
+        {
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet);
+            Excel.Range firstRow = activeWorksheet.get_Range(Adres);
+            firstRow.EntireRow.Insert(Excel.XlInsertShiftDirection.xlShiftDown);
+            Excel.Range newFirstRow = activeWorksheet.get_Range(Adres);
+            newFirstRow.Value2 = tekst;
+        }
+
+        private void ThisWorkbook_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisWorkbook_Startup);
+            this.Shutdown += new System.EventHandler(ThisWorkbook_Shutdown);
+        }
+
+        #endregion
+
+    }
+}
